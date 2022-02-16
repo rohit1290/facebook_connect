bc8e493160cff8648ba9bfeb3e6082751ee75972
Rohit Gupta <rohit_1290@yahoo.com>
2022-02-16 23:50:50 +0530

    update: changed elgg_unset_plugin_user_setting to removePluginSetting

--- views/default/resources/facebook_connect/revoke.php
+++ views/default/resources/facebook_connect/revoke.php
@@ -2,8 +2,8 @@
 $user = elgg_get_logged_in_user_entity();
 
 // unregister user's information
-elgg_unset_plugin_user_setting('fbid', $user->guid, 'facebook_connect');
-elgg_unset_plugin_user_setting('fbaccess_token', $user->guid, 'facebook_connect');
-// elgg_unset_plugin_user_setting('fbname', $user->guid, 'facebook_connect');
+$user->removePluginSetting('facebook_connect', 'fbid');
+$user->removePluginSetting('facebook_connect', 'fbaccess_token');
+// $user->removePluginSetting('facebook_connect', 'fbname');
 
 return elgg_ok_response('', elgg_echo('facebook_connect:revoke:success'), 'settings/plugins/'.$user->username.'/facebook_connect');
